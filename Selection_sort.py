e13753f93dc6976822a4081defc4ac296c0402ac
Parthiv-06 <puthalapattuparthiv@gmail.com>
2025-05-12 14:46:17 +0530

    this is a selection sorting technique

---
+++ Selection_sort.py
@@ -0,0 +1,12 @@
+def selection_sort(arr):
+    n=len(arr)
+    for i in range(n):
+        mini=i
+        for j in range(i+1,n):
+            if arr[j]<arr[mini]:
+                mini=j
+        arr[i],arr[mini]=arr[mini],arr[i]
+        print(arr)
+    return arr
+arr=[5,17,13,20,6,12]
+print(selection_sort(arr))
